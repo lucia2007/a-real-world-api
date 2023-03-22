eaf1160b1ee66109fcb2477a9e67dc378bcb3276
Lucia Ferencik <lferencikova@gmail.com>
2023-03-22 10:45:30 +0000

    Bug: Comma separate the options

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,28 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+
+function processOptions(form) {
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options");
+    form.append("options", optArray.join());
+
+    return form;
+}
+
+
 async function postForm(e) {
 
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
-    // for (let e of form.entries()) {
-    //     console.log(e);
+    // for (let entry of form.entries()) {
+    //     console.log(entry);
     // }
 
     const response = await fetch(API_URL, {
